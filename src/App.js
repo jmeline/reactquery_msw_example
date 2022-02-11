1f038ac85a68f908a4fcf01098178c690185b34c
Jacob Meline <jacob.meline@extendhealth.com>
2022-02-11 14:23:29 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useQuery } from "react-query"
+
+const fetchCatFacts = async () => 
+  await fetch("/facts")
+      .then(response => response.json())
+  // await fetch("https://cat-fact.herokuapp.com/facts")
+
+export const useCatFacts = () => {
+  const catfacts = useQuery('cats', fetchCatFacts)
+  const facts = catfacts.data
+    ?.map((x, idx) => <div key={idx}>{x.text}</div>)
+    ?? []
+  return {catfacts, facts}
+}
+
 
 function App() {
+  const { catfacts, facts} = useCatFacts()
+  if (catfacts.isLoading) {
+    return <div>Loading... random cat nonsense</div>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React by becoming a cat
+          </a>
+          {facts}
+        </header>
+      </div>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,63 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, waitFor } from '@testing-library/react';
+import {renderHook} from "@testing-library/react-hooks"
+import {setupServer} from "msw/node"
+import {rest} from "msw"
+import App, {useCatFacts} from './App';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const server = setupServer()
+const client = new QueryClient()
+
+beforeAll(() => server.listen({onUnhandledRequest: "warn"}))
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+test('renders learn react link', async() => {
+  server.use(
+    // rest.get("https://cat-fact.herokuapp.com/facts", (req, res, ctx) => {
+    rest.get("/facts", (req, res, ctx) => {
+      console.log("I HIT SOMETHING!!!")
+      return res(
+        ctx.status(200),
+        ctx.json([
+          { "text": "Dogs are better" },
+          { "text": "Dogs are more loving and less evil than cats" }
+        ]))
+    })
+  )
+  render(
+    <QueryClientProvider client={client}>
+      <App />
+    </QueryClientProvider>
+  );
+  // screen.debug()
+  expect(await screen.findByText(/Loading/i)).toBeInTheDocument();
+  expect(await screen.findByText("Dogs are better")).toBeInTheDocument();
 });
+
+test('useCatfActs hook', async () => {
+  const expectedResult = [
+    { "text": "Dogs are better" },
+    { "text": "Dogs are more loving and less evil than cats" }
+  ]
+  server.use(
+    // rest.get("https://cat-fact.herokuapp.com/facts", (req, res, ctx) => {
+    //   console.log("I HIT SOMETHING!!!")
+    //   return res(
+    //     ctx.status(200),
+    //     ctx.json(expectedResult))
+    // })
+  )
+  const wrapper = ({children}) => 
+    <QueryClientProvider client={client}>
+      {children}
+    </QueryClientProvider>
+
+  const {result, waitFor} = renderHook(() => useCatFacts(), { wrapper })
+
+  await waitFor(() => result.current.catfacts.isSuccess)
+
+  const facts = result.current.catfacts.data.map(x => x.text)
+  expect(facts).toEqual(expectedResult.map(x => x.text))
+
+})
